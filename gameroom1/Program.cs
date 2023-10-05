a306563cd736083e7391d48f81662f6a3279964e
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-10-05 19:09:56 +0200

    hv

--- gameroom1/Program.cs
+++ gameroom1/Program.cs
@@ -1,18 +1,30 @@
 //dec
 bool startgame = false;
+bool alive = true;
 
 Console.WriteLine("> hello person! ready to enter the game?\n>> (y/n)");
-do{
+do
+{
     string yn = Console.ReadLine();
-    if (yn == "y"){
-        Console.WriteLine("> got it. let's begin!");
+    if (yn == "y")
+    {
+        Console.WriteLine("> got it.");
         startgame = true;
     }
-    else if (yn == "n"){
+    else if (yn == "n")
+    {
         Console.WriteLine("> alright... ready now?\n>> (y/n)");
     }
-    else if (yn != "y" || yn != "n"){
+    else if (yn != "y" || yn != "n")
+    {
         Console.WriteLine("> invalid input :(\n>> (y/n)");
     }
 }
 while (!startgame);
+Console.WriteLine("> let's begin.");
+
+do
+{
+
+}
+while (alive);
