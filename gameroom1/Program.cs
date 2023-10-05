2ad8bf3a86af16c239f164b0847ed79d65616e71
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-10-05 14:44:01 +0200

    game yn start

--- gameroom1/Program.cs
+++ gameroom1/Program.cs
@@ -1,44 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+//dec
+bool startgame = false;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+Console.WriteLine("> hello person! ready to enter the game?\n>> (y/n)");
+do{
+    string yn = Console.ReadLine();
+    if (yn == "y"){
+        Console.WriteLine("> got it. let's begin!");
+        startgame = true;
+    }
+    else if (yn == "n"){
+        Console.WriteLine("> alright... ready now?\n>> (y/n)");
+    }
+    else if (yn != "y" || yn != "n"){
+        Console.WriteLine("> invalid input :(\n>> (y/n)");
+    }
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+while (!startgame);
