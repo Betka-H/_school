f5d437894cbd24063551e6c72bf6e1924c34f7da
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-10-05 09:44:47 +0200

    ewffsdfs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,13 +1,8 @@
 //dotnet run --project WebApplication1
 
+
 Console.WriteLine("> enter pw:");
-
-
-
 string pw = Console.ReadLine();
-
-
-
 if (pw == "mypassword"){
     Console.WriteLine("> fine");
 }
@@ -18,3 +13,24 @@ Console.WriteLine("> what's your name?");
 string uname = Console.ReadLine();
 Console.WriteLine("> splendid, hi " + uname + "!\n> how old are you?");
 
+
+
+
+string uinputage = Console.ReadLine();
+int uage = 0;
+bool agevalid = int.TryParse(uinputage, out uage);
+
+for (int lowage = 18; uage > lowage; lowage = lowage+1){
+    Console.WriteLine(lowage);
+}
+
+//the holy grail vvvvv
+
+bool parse;
+do{
+    Console.WriteLine("> give me a number");
+    string unumber = Console.ReadLine();
+    int parsednumber = 0;
+    parse = int.TryParse(unumber, out parsednumber);
+}
+while (!parse);
