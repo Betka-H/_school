e500aa639b1eef17b6f7b4a09aa929e64b14728a
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-10-05 08:07:03 +0200

    dvbsdjvbsx

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,1 +1,47 @@
-Console.WriteLine("helo worl");
+//dotnet run --project WebApplication1
+
+Console.WriteLine("> hello, user. please state your name.");
+string uname = Console.ReadLine().ToLower();
+Console.WriteLine("> welcome, " + uname + "\non a scale of \"good\" to \"potatoes\", how are you doing today?");
+string umood = Console.ReadLine().ToLower();
+if (umood == "potatoes"){
+    Console.WriteLine("> sorry to hear that");
+}
+else if (umood == "good"){
+    Console.WriteLine("> good!");
+}
+else {
+    Console.WriteLine("> okay...");
+}
+
+
+string yn;
+
+
+do
+        {
+            Console.WriteLine("> ready to begin?\ny/n");
+            yn = Console.ReadLine();
+
+            if (yn == "n"){
+                Console.WriteLine("> alright...");
+            }
+
+            if ( ( yn != "y" ) && ( yn != "n" )){
+                Console.WriteLine("> invalid input!");
+            }
+            
+
+        } while (yn != "y");
+
+        Console.WriteLine("alright!");
+
+
+
+
+
+
+
+
+
+
