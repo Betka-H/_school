d62a62e3f45fabb00cc8ad14f74c970d55e95ceb
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-11-02 15:20:18 +0100

    idk whats supposed to be calculated in  ukol1

--- ukol1/Program.cs
+++ ukol1/Program.cs
@@ -22,8 +22,16 @@ int userNumber()
 }
 
 Console.Write("enter integer a: ");
+int a = userNumber();
 Console.WriteLine("a = " + userNumber());
 Console.Write("enter integer b: ");
+int a = userNumber();
+
 Console.WriteLine("b = " + userNumber());
 Console.Write("enter integer c: ");
+int a = userNumber();
+
 Console.WriteLine("c = " + userNumber());
+Console.Write("enter integer x: ");
+int x = userNumber();
+Console.WriteLine("x = " + userNumber());
