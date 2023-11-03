e8e0f237db1ebd2b3b9512a9b0b5a19c5296a4df
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-11-03 16:20:48 +0100

    purged.

--- ukol1/Program.cs
+++ ukol1/Program.cs
@@ -1,37 +1,111 @@
 // kvadraticka rovnice
 // ax^2 + bx + c = 0
 // x1,2 = +-Vb^2-4ac / 2a
+// b^2-4ac >= 0
 // uzivatel zada a, b, c
 // pouzit funkci!
 // na odmocninu V knihovna Math.Sqrt(69); MathF for floats
 // poslat odkaz na github
+/*
+Po zadání program vypíše rovnici, počet řešení a jaká jsou.
+Program musí fungovat pro jakékoli koeficienty a, b, c (i necelé).
+Následně program dovolí uživateli dosazovat do funkce hodnotu x, dokud uživatel nezadá ukončující vstup.
+ */
 
-int userNumber()
+bool repeat, yes = false, success;
+string userString;
+double a, b, c, x, D, result;
+
+// gets user input, specifically a number
+double userNumber()
 {
-    int userNumber;
-    bool inputParse;
+    double userNumber;
     do
     {
-        inputParse = Int32.TryParse(Console.ReadLine(), out userNumber);
-        if (!inputParse)
+        userString = Console.ReadLine().ToLower();
+        terminate();
+        success = double.TryParse(userString, out userNumber);
+        if (!success)
         {
-            Console.Write("thats not a number. please input a number: ");
+            Console.Write("that's not a number. please input a number: ");
         }
-    } while (!inputParse);
+    } while (!success);
     return userNumber;
 }
+// terminate the program
+void terminate()
+{
+    if (userString == "exit")
+    {
+        Console.WriteLine("terminating program...");
+        Environment.Exit(0);
+    }
+}
+// true/false depending on the user input (y/n)
+bool yn()
+{
+    do
+    {
+        userString = Console.ReadLine().ToLower();
+        terminate();
+        switch (userString)
+        {
+            case "y":
+                yes = true;
+                success = true;
+                break;
+            case "n":
+                yes = false;
+                success = true;
+                break;
+            default:
+                Console.WriteLine("invalid input : (y/n) expected.");
+                success = false;
+                break;
+        }
+    } while (!success);
+    return yes;
+}
 
-Console.Write("enter integer a: ");
-int a = userNumber();
-Console.WriteLine("a = " + userNumber());
-Console.Write("enter integer b: ");
-int a = userNumber();
+Console.WriteLine("this program will solve ax^2 + bx + c = 0. input \"exit\" at any time to terminate the program");
+do
+{
+    Console.Write("enter number a: ");
+    a = userNumber();
+    if (a == 0)
+    {
+        Console.Write("\"a\" cannot equal zero! please input a different number: ");
+        a = userNumber();
+    }
+    Console.Write("enter number b: ");
+    b = userNumber();
+    Console.Write("enter number c: ");
+    c = userNumber();
+    D = Math.Pow(b, 2) - 4 * a * c;
 
-Console.WriteLine("b = " + userNumber());
-Console.Write("enter integer c: ");
-int a = userNumber();
+    Console.WriteLine($"the equation is: {a}x^2 + {b}x + {c} = 0");
 
-Console.WriteLine("c = " + userNumber());
-Console.Write("enter integer x: ");
-int x = userNumber();
-Console.WriteLine("x = " + userNumber());
+    if (b == 0 && c == 0)
+    {
+        Console.WriteLine("there is an infinite amount of answers for \"x\"");
+    }
+
+    switch (D)
+    {
+        case < 0:
+            Console.WriteLine("there is no answer for \"x\"");
+            break;
+        case 0:
+            Console.WriteLine("there is one answer for \"x\": ");
+
+            break;
+        case > 0:
+            Console.WriteLine("there are two answers for \"x\": ");
+
+            break;
+    }
+
+
+    Console.Write("do you want to repeat the program? y/n: ");
+    repeat = yn();
+} while (repeat);
--- useful.cs
+++ useful.cs
@@ -4,51 +4,10 @@
 
 
 
-
-
-Console.Write("h"); //dont end with a new line
-Console.WriteLine("elo worl"); //end with a new line
-
-int num1 = 2; //integers
-int num2 = 5;
-double des = 2.14; //decimal
-string name = "sbgjasgdsgg"; //string
-bool yes = true; //boolean
-
-
-Console.WriteLine("hello there, " + name); // "append" a string to wl (writeline) text
-
-int add = num1 + num2;
-
-Console.WriteLine(add);
-
-double dbl = des + num1;
-int dblint = (int)dbl; //convert double to int
-
-Console.WriteLine(dblint); //prints it ig
-
-if (yes)
-{
-    Console.WriteLine("bool yes is true");
-}
-
-if (!yes)
-{
-    Console.WriteLine("bool yes is false");
-}
-
-
-bool compb = num1 == 7; //compares x to 7 and outputs the bool
-Console.WriteLine(compb);
-
-
-
-//string shenanigans
-
+// OLD
 Console.WriteLine(">>> tell me the name of god you fungal piece fo shit");
 string askm = Console.ReadLine().ToLower(); //converts string to lower
-
-// vvv use "==", not "="!
+                                            // vvv use "==", not "="!
 if (askm == "you cannot kill me in a way that matters")
 {
     Console.WriteLine(">>> decay exists as an extant form of life");
@@ -57,79 +16,6 @@ else
 {
     Console.WriteLine(">>> wrong answer bozo");
 }
-
-//
-//
-//OLD
-//
-//
-
-
-
-//number shenanigans
-
-Console.WriteLine("zadej cislo");
-string uin = Console.ReadLine();
-int uinreal;
-bool uinsucess = int.TryParse(uin, out uinreal);
-
-if (uinsucess == true)
-{
-    Console.WriteLine("suces");
-}
-else
-{
-    Console.WriteLine("not a number");
-}
-
-
-
-//yn shenanigans
-
-Console.WriteLine("> hello, user. please state your name.");
-string uname = Console.ReadLine().ToLower();
-Console.WriteLine("> welcome, " + uname + "\non a scale of \"good\" to \"potatoes\", how are you doing today?");
-string umood = Console.ReadLine().ToLower();
-if (umood == "potatoes")
-{
-    Console.WriteLine("> sorry to hear that");
-}
-else if (umood == "good")
-{
-    Console.WriteLine("> good!");
-}
-else
-{
-    Console.WriteLine("> okay...");
-}
-
-
-string yn;
-
-
-do
-{
-    Console.WriteLine("> ready to begin?\ny/n");
-    yn = Console.ReadLine();
-
-    if (yn == "n")
-    {
-        Console.WriteLine("> alright...");
-    }
-
-    if ((yn != "y") && (yn != "n"))
-    {
-        Console.WriteLine("> invalid input!");
-    }
-
-
-} while (yn != "y");
-
-Console.WriteLine("alright!");
-
-
-
-//pw/age shenanigans
 
 Console.WriteLine("> enter pw:");
 string pw = Console.ReadLine();
@@ -144,71 +30,32 @@ else
 Console.WriteLine("> what's your name?");
 string uname = Console.ReadLine();
 Console.WriteLine("> splendid, hi " + uname + "!\n> how old are you?");
+// OLD
 
 
 
-
-string uinputage = Console.ReadLine();
-int uage = 0;
-bool agevalid = int.TryParse(uinputage, out uage);
-
-for (int lowage = 18; uage > lowage; lowage = lowage + 1)
+switch (foo) // switch
 {
-    Console.WriteLine(lowage);
-}
-
-//the holy grail vvvvv
-
-bool parse;
-do
-{
-    Console.WriteLine("> give me a number");
-    string unumber = Console.ReadLine();
-    int parsednumber = 0;
-    parse = int.TryParse(unumber, out parsednumber);
-}
-while (!parse);
-
-bool isseven = x == 7; //is true depending on whether x == 7 or not
-
-
-switch (foo)
-{
-    case "value1":
-        Console.WriteLine("bjdkashfjlaksfhalksjhf");
+    case x:
+        // output 1
         break;
-    case "value2":
-        Console.WriteLine("habdkajsbhasbgbdshdbgs");
+    case y:
+        // output 2
+        break;
+    case default:
+        // output when every other check fails
         break;
 }
 
-//yn module
-do
-{
-    userInput = Console.ReadLine().ToLower();
-    switch (userInput)
-    {
-        case "y":
-            break;
-        case "n":
-            Console.WriteLine("answered no");
-            //Environment.Exit(0);
-            break;
-        default:
-            Console.WriteLine("> invalid input : (y/n) expected.");
-            Console.WriteLine("question repeat");
-            break;
-    }
-} while (userInput != "y");
+Console.WriteLine(DateTime.Now); // time
 
-Console.WriteLine(DateTime.Now);
+Console.ForegroundColor = ConsoleColor.Gray; // color
 
-Console.ForegroundColor = ConsoleColor.Gray;
 
 
 // functions
 Console.WriteLine(maximum(16, 42));
-float maximum(float x, float y)
+float maximum(float x, float y) // needs at least 1 return value
 {
     if (x > y)
     {
@@ -227,7 +74,7 @@ float maximum(float x, float y)
 
 
 
-void userNumber()
+void userNumber() // void needs no return value
 {
     int userNumber;
     bool inputParse;
@@ -235,56 +82,7 @@ void userNumber()
         Console.Write("please input your number: ");
         inputParse = Int32.TryParse(Console.ReadLine(), out userNumber);
     } while (!inputParse) ;
-
     Console.WriteLine("your number is: " + userNumber);
-
 }
 
-userNumber();
-
-
-
-
-
-for (int i = 1; i <= w3CategoryAmount; i++)
-{
-    w3CatRandom = rnd.Next(w3Categories.Count()); // generates a random number
-    if (!w3CategoriesChosenIndex.Contains(w3CatRandom)) // // only run if the random number isnt already in the array
-    {
-        // number part
-        Array.Resize(ref w3CategoriesChosenIndex, i); // resizes repeat check array +1
-        w3CategoriesChosenIndex[i - 1] = w3CatRandom; // writes the random number into the freed up space
-        Console.Write(">>>>> category " + i + ": "); // prints category number
-        // category part
-        Array.Resize(ref w3CategoriesChosen, i); // resizes chosen category list array +1
-        w3CategoriesChosen[i - 1] = w3Categories[w3CatRandom]; // writes the randomly chosen category into the freed up space
-        Console.WriteLine(w3Categories[w3CatRandom] + " (" + w3CatRandom + ")"); // prints category name
-        // item part
-        for (int j = 1; j <= w3ItemAmount; j++)
-        {
-            w3ItemRandom = rnd.Next(w3AllItems[w3CatRandom].Count()); // generates random item number
-            Array.Resize(ref w3ItemsChosenIndex, j); // resizes repeat check array +1
-            w3ItemsChosenIndex[j - 1] = w3ItemRandom; // writes the random number into the freed up space
-            Console.WriteLine(">> item index: " + w3ItemRandom); // prints item index
-            //Console.WriteLine(w3AllItems[2][4]);
-            if (!w3CategoriesChosenIndex.Contains(w3ItemRandom))
-            {
-                Array.Resize(ref w3ItemsChosenIndex, j); // resizes repeat check array +1
-                w3ItemsChosenIndex[j - 1] = w3ItemRandom; // writes the random number into the freed up space
-                Console.WriteLine("> item: " + w3AllItems[w3CatRandom][w3ItemRandom]); // prints item number
-                Array.Resize(ref w3ItemsChosen, i); // resizes chosen item list array +1
-                w3ItemsChosen[j - 1] = w3AllItems[w3CatRandom][w3ItemRandom]; // writes the randomly chosen item into the freed up space
-            }
-            else
-            {
-                Console.WriteLine("debug>> item number " + "(" + w3ItemRandom + ")" + " already in list. skipping."); // debug
-                j--; // so that the loop can run again correctly
-            }
-        }
-    }
-    else // if the random number already is in the array
-    {
-        Console.WriteLine("debug>> category number " + "(" + w3CatRandom + ")" + " already in list. skipping."); // debug
-        i--; // so that the loop can run again correctly
-    }
-}
+Console.WriteLine($"text, but with {variables}! how {cool} is that??");//interpolated or whatnot
