7836c18cb39c62fd08996db5ee0d161e52c4c673
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-11-12 23:20:52 +0100

    final

--- ukol1/Program.cs
+++ ukol1/Program.cs
@@ -11,20 +11,27 @@ void detectAndTerminate()
 }
 float getNumber()
 {
-    do
+    while (true)
     {
         userString = Console.ReadLine().ToLower();
         detectAndTerminate();
         if (float.TryParse(userString, out userNumber))
         {
-            break;
+            return userNumber;
         }
         Console.Write("that's not a number. please input a number: ");
-    } while (true);
-    return userNumber;
+    }
+}
+float x1()
+{
+    return MathF.Round(x = (-b + MathF.Sqrt(D)) / (2 * a), 2);
+}
+float x2()
+{
+    return MathF.Round(x = (-b - MathF.Sqrt(D)) / (2 * a), 2);
 }
 
-do
+while (true)
 {
     Console.WriteLine("this program will solve ax^2 + bx + c = 0. input \"e\" at any time to exit");
     Console.Write("enter number a: ");
@@ -34,9 +41,11 @@ do
     Console.Write("enter number c: ");
     c = getNumber();
 
+    Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+
     if (a == 0 && b == 0 && c == 0)
     {
-        Console.WriteLine($"there is an infinite amount of x for {a}x^2 + {b}x + {c} = 0");
+        Console.WriteLine($"there is an infinite amount of x values for {a}x^2 + {b}x + {c} = 0");
     }
     else
     {
@@ -54,19 +63,19 @@ do
                 break;
             case 0:
                 Console.WriteLine("there is one answer for x: ");
-                Console.WriteLine("x = " + Math.Round(x = (-b + MathF.Sqrt(D)) / (2 * a), 2));
+                Console.Write($"x = {x1()}");
                 break;
             case > 0:
                 Console.WriteLine("there are two answers for x: ");
-                Console.WriteLine("x1 = " + Math.Round(x = (-b + MathF.Sqrt(D)) / (2 * a), 2));
-                Console.WriteLine("x2 = " + Math.Round(x = (-b - MathF.Sqrt(D)) / (2 * a), 2));
+                Console.WriteLine($"x1 = {x1()}");
+                Console.WriteLine($"x2 = {x2()}");
                 break;
         }
 
         // evaluate x
-        do
+        while (true)
         {
-            Console.Write("enter parameter x for evaluation (enter \"s\" to stop evaluating): ");
+            Console.Write("enter parameter x for evaluation (or enter \"s\" to stop evaluating): ");
             userString = Console.ReadLine().ToLower();
             detectAndTerminate(); // detects "exit" keyword
             if (userString == "s")
@@ -75,17 +84,16 @@ do
             }
             if (float.TryParse(userString, out x))
             {
-                Console.WriteLine($"the equation is {a}*({x})^2 + {b}*({x}) + {c} = {Math.Round(a * MathF.Pow(x, 2) + b * x + c, 2)}");
+                Console.WriteLine($"the equation is {a}*({x})^2 + {b}*({x}) + {c} = {MathF.Round(a * MathF.Pow(x, 2) + b * x + c, 2)}");
             }
             else
             {
-                Console.Write("that's not a number. please ");
+                Console.Write("invalid input. please ");
             }
-        } while (true);
+        }
     }
-
     // end
-    do
+    while (true)
     {
         Console.Write("do you want to repeat the program? \"r\" to repeat, \"e\" to exit: ");
         userString = Console.ReadLine().ToLower();
@@ -95,5 +103,5 @@ do
             break;
         }
         Console.Write("invalid input; ");
-    } while (true);
-} while (true);
+    }
+}
